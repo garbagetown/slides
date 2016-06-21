1156ee7bbdd45ccb5ff9d1dd0da49c14b5f0f112
yuichiro umezawa <garbagetown@gmail.com>
2016-06-21 13:51:46 +0900

    add User mvc features

---
+++ sample/play2/play1sample/app/models/User.java
@@ -0,0 +1,20 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import play.db.jpa.GenericModel;
+import play.db.jpa.Model;
+
+@Entity
+public class User extends GenericModel {
+
+    @Id
+    @GeneratedValue
+    public String id;
+    public String email;
+    public String password;
+    public String fullname;
+    public boolean isAdmin;
+}
